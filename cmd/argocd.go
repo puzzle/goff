0e803b321f78e4d1014d846066b4bc4bd7988e8e
Christian Schlatter <schlatter@puzzle.ch>
2023-05-15 07:03:51 +0200

    add split command and git auth

--- cmd/argocd.go
+++ cmd/argocd.go
@@ -13,6 +13,9 @@ import (
 var repoServerUrl *string
 
 var argoOutputDir *string
+var repoUsername *string
+var repoPassword *string
+var repoSshKey *string
 
 // argocdCmd represents the argocd command
 var argocdCmd = &cobra.Command{
@@ -21,21 +24,20 @@ var argocdCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Long:  `Render manifests from ArgoCD Application`,
 	Run: func(cmd *cobra.Command, args []string) {
-		argocd.Render(args[0], *repoServerUrl, *argoOutputDir)
+		argocd.Render(args[0], *repoServerUrl, *argoOutputDir, argocd.RepoCredentails{
+			Username: *repoUsername,
+			Password: *repoPassword,
+			KeyFile:  *repoSshKey,
+		})
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(argocdCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// argocdCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	repoServerUrl = argocdCmd.Flags().String("repoServer", "localhost:8081", "URL to argoCD repo server")
 	argoOutputDir = argocdCmd.Flags().StringP("output-dir", "o", ".", "Output directory")
+	repoUsername = argocdCmd.Flags().StringP("username", "u", "", "Repo username")
+	repoPassword = argocdCmd.Flags().StringP("password", "p", "", "Repo password")
+	repoSshKey = argocdCmd.Flags().StringP("ssh-key", "i", "", "Repo SSH Key")
 }
