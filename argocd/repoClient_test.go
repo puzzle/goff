978fa2fbc82853b6b57ccae996c56ccd123d8aec
Christian Schlatter <schlatter@puzzle.ch>
2023-06-30 19:13:32 +0200

    fix ci

--- argocd/repoClient_test.go
+++ argocd/repoClient_test.go
@@ -3,7 +3,7 @@ package argocd
 import "testing"
 
 func TestRender(t *testing.T) {
-	err := Render("../testdata/argocd/helm_app.yaml", "localhost:8081", "../out/argocd/app/", RepoCredentails{})
+	err := Render("../testdata/argocd/helm_app.yaml", "reposerver:8081", "../out/argocd/app/", RepoCredentails{})
 	if err != nil {
 		t.Error(err)
 	}
