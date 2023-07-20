5d975b863f3ed34186cd32337b3df07e061c2ae4
Christian Schlatter <schlatter@puzzle.ch>
2023-07-20 07:23:42 +0200

    fix tests

--- argocd/repoClient_test.go
+++ argocd/repoClient_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestRender(t *testing.T) {
 	//deactivate until https://github.com/dagger/dagger/issues/5382 is fixed
-	err := Render("../testdata/argocd/helm_app.yaml", "localhost:8081", "../out/argocd/app/", RepoCredentails{})
+	err := Render("../testdata/argocd/helm_app.yaml", "reposerver:8081", "../out/argocd/app/", RepoCredentails{})
 	if err != nil {
 		t.Error(err)
 	}
